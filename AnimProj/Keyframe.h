50d72f5e7625b23d3484db2e3ce57855844008df
yudaegam <dbckddhks13@gmail.com>
2023-07-17 16:18:54 +0900

    Ready for refactoring Animation... Create Keyframe class for new version of Animation

---
+++ AnimProj/Keyframe.h
@@ -0,0 +1,22 @@
+// author: Changwan Yu
+#pragma once
+
+namespace pa
+{
+	class Keyframe
+	{
+	public:
+		enum class Channel : uint16_t
+		{
+			Scale, Rotation, Translation, None
+		};
+	public:
+		uint16_t			_keytime	= std::numeric_limits<uint16_t>::max();
+		uint16_t			_boneIndex	= std::numeric_limits<uint16_t>::max();
+		Channel				_channel	= Channel::None;
+
+		DirectX::XMFLOAT4	_v;
+	};
+}
+
+
