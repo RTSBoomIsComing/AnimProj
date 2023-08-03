db1f320d06398ac86d53dd3a30f18412d73a9da9
yudaegam <dbckddhks13@gmail.com>
2023-08-03 18:59:17 +0900

    In progress of implementing MatrixPool

--- AnimProj/Common/MatrixPool.h
+++ AnimProj/Common/MatrixPool.h
@@ -5,17 +5,23 @@ namespace pa
 	class MatrixPool
 	{
 	public:
-		MatrixPool(size_t stride, size_t capacity);
+		MatrixPool(std::vector<DirectX::XMFLOAT4X4>& pool, size_t stride, size_t capacity);
 		~MatrixPool();
 
 	public:
 		DirectX::XMFLOAT4X4* acquire();
-		void release(DirectX::XMFLOAT4X4* pData);
+		void release(DirectX::XMFLOAT4X4* ptr);
 
 	private:
-		//void*								_owner;
-		size_t								_stride	 = 1;
-		std::vector<DirectX::XMFLOAT4X4>	_pool;
+		size_t getID(DirectX::XMFLOAT4X4* ptr) const;
+		size_t size() const;
+
+	private:
+		const size_t						_stride		= 1;
+		std::vector<DirectX::XMFLOAT4X4>&	_pool;
+		std::vector<size_t>					_ownerIDs;
+
+		void (*reOwnership)(size_t ownerID, DirectX::XMFLOAT4X4* newPtr);	// 이거 맞아??? 이렇게까지 해야해???
 	};
 }
 
