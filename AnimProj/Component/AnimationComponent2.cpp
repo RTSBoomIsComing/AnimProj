947be3d6f57e1c9f8ac633fde089c4d2e96c64a4
yudaegam <dbckddhks13@gmail.com>
2023-08-14 16:18:06 +0900

    Implement AnimationComponent2

--- AnimProj/Component/AnimationComponent2.cpp
+++ AnimProj/Component/AnimationComponent2.cpp
@@ -2,20 +2,155 @@
 #include "pch.h"
 #include "AnimationComponent2.h"
 #include "../Animation/Animation.h"
+#include "../World/World.h"
+#include "../Actor/Actor.h"
+#include "../Component/SkeletalMeshComponent.h"
 
 namespace pa
 {
 	void AnimationComponent2::onUpdate(World& world, Actor& owner, float deltaTime)
 	{
+		_upperBody.onUpdate(deltaTime);
+		_lowerBody.onUpdate(deltaTime);
+
+		// Blend animation layer
+		SkeletalMeshComponent* skeletalMeshComp = owner.getComponent<SkeletalMeshComponent>();
+
+		for (int trackID = 0; trackID < _lowerBody.activeKeys.size(); trackID++)
+		{
+			const uint16_t boneID = _lowerBody.animation->getTrackHeaders()[trackID].boneID;
+			if (AnimationTrackType::Rotation == _lowerBody.animation->getTrackHeaders()[trackID].type)
+			{
+				skeletalMeshComp->_pose[boneID] = _lowerBody.currentPose[boneID];
+			}
+		}
+
+		for (int trackID = 0; trackID < _upperBody.activeKeys.size(); trackID++)
+		{
+			const uint16_t boneID = _upperBody.animation->getTrackHeaders()[trackID].boneID;
+			if (AnimationTrackType::Rotation == _upperBody.animation->getTrackHeaders()[trackID].type)
+			{
+				skeletalMeshComp->_pose[boneID] = _upperBody.currentPose[boneID];
+			}
+		}
 	}
+
 	void AnimationComponent2::transitAnimationUpperBody(const Animation& animation, float transitionTime)
 	{
-		_upperBody.activeKeys.resize(animation.getTrackHeaders().size());
-		initializeActiveKeysWithMemCopy();
+		_upperBody.transitAnimation(animation, transitionTime);
 	}
+
 	void AnimationComponent2::transitAnimationLowerBody(const Animation& animation, float transitionTime)
 	{
 		_lowerBody.activeKeys.resize(animation.getTrackHeaders().size());
-		
+	}
+
+	void AnimationComponent2::Layer::transitAnimation(const Animation& animation, float transitionTime)
+	{
+		this->animation		 = &animation;
+		this->transitionTime = transitionTime;
+		this->playTime		 = 0.0f;
+
+		activeKeys.resize(this->animation->getTrackHeaders().size());
+		lastPose.swap(currentPose);
+		currentPose.clear();
+		currentPose.resize(lastPose.size());
+
+		const size_t trackCount = this->animation->getTrackHeaders().size();
+		cursor					= trackCount * 4;
+		std::memcpy(activeKeys.data(), this->animation->getKeyframes().data(), sizeof(CompactKey) * cursor);
+	}
+
+	void AnimationComponent2::Layer::onUpdate(float deltaTime)
+	{
+		using namespace DirectX;
+		constexpr int fps = 120;
+
+		if (nullptr == animation)
+		{
+			// TODO: Notify End animation
+			return;
+		}
+
+		playTime += deltaTime;
+		float runningFrame = playTime * fps;
+
+		const uint16_t duration = animation->getKeyframes().back().keytime;
+		if (duration < runningFrame)
+		{
+			animation = nullptr;
+			//runningFrame = duration;
+			return;
+		}
+
+		while (cursor < animation->getKeyframes().size())
+		{
+			uint16_t trackID	   = animation->getTrackIDs()[cursor];
+			auto&	 controlPoints = activeKeys[trackID];
+			if (controlPoints[2].keytime < runningFrame)
+			{
+				controlPoints[0] = controlPoints[1];
+				controlPoints[1] = controlPoints[2];
+				controlPoints[2] = controlPoints[3];
+				controlPoints[3] = animation->getKeyframes()[cursor];
+
+				cursor++;
+			}
+			else
+				break;
+		}
+
+		//cache pose
+
+		float weight = playTime / transitionTime;
+		weight		 = std::min(weight, 1.0f);
+
+		for (int trackID = 0; trackID < activeKeys.size(); trackID++)
+		{
+			const uint16_t boneID		 = animation->getTrackHeaders()[trackID].boneID;
+			const auto&	   controlPoints = activeKeys[trackID];
+
+			if (controlPoints[1].keytime >= controlPoints[2].keytime)
+				DebugBreak();
+
+			if (controlPoints[1].keytime > runningFrame || runningFrame > controlPoints[2].keytime)
+				DebugBreak();
+
+			float t = (runningFrame - controlPoints[1].keytime) / (controlPoints[2].keytime - controlPoints[1].keytime);
+
+			t = std::min(1.0f, t);
+
+			if (AnimationTrackType::Rotation == animation->getTrackHeaders()[trackID].type)
+			{
+				XMVECTOR Q1 = XMQuaternionSlerp(
+					controlPoints[1].decompressAsQuaternion(),
+					controlPoints[2].decompressAsQuaternion(), t);
+
+				if (1.0f == weight)
+				{
+					XMStoreFloat4(&currentPose[boneID].rotation, Q1);
+					//XMStoreFloat4(&skeletalMeshComp->_pose[boneID].rotation, Q1);
+				}
+				else
+				{
+					XMVECTOR Q0		 = XMLoadFloat4(&lastPose[boneID].rotation);
+					XMVECTOR Qresult = XMQuaternionSlerp(Q0, Q1, weight);
+					XMStoreFloat4(&currentPose[boneID].rotation, Qresult);
+					//XMStoreFloat4(&basePose[boneID].rotation, Qresult);
+					//XMStoreFloat4(&basePose[boneID].rotation, Qresult);
+				}
+
+				//_rotations[_animation->_trackDescriptors[i]] = cp[1].decompressAsQuaternion();
+
+				//_rotations[_animation->_trackDescriptors[i]] =
+				//	XMVectorLerp(cp[1].decompressAsQuaternion(), cp[2].decompressAsQuaternion(), weight);
+
+				//_rotations[boneID] = XMQuaternionNormalize(XMVectorCatmullRom(
+				//	controlPoints[0].decompressAsQuaternion(),
+				//	controlPoints[1].decompressAsQuaternion(),
+				//	controlPoints[2].decompressAsQuaternion(),
+				//	controlPoints[3].decompressAsQuaternion(), weight));
+			}
+		}
 	}
 }
--- AnimProj/Component/AnimationComponent2.h
+++ AnimProj/Component/AnimationComponent2.h
@@ -11,20 +11,34 @@ namespace pa
 	{
 		struct Layer
 		{
+			Layer()
+			{
+				lastPose.resize(50 /* hard coded, need fix */);
+				currentPose.resize(50 /* hard coded, need fix */);
+			}
+
 			std::vector<std::array<CompactKey, 4>> activeKeys;
-			size_t								   cursor	 = 0;
-			const Animation*					   animation = nullptr;
-			float								   playTime	 = 0.0f;
+			const Animation*					   animation	  = nullptr;
+			size_t								   cursor		  = 0;
+			float								   playTime		  = 0.0f;
 			float								   transitionTime = 0.0f;
+
+			void transitAnimation(const Animation& animation, float transitionTime = 1.0f);
+			void onUpdate(float deltaTime);
+
+			std::vector<Transform> lastPose;
+			std::vector<Transform> currentPose;
 		};
 
 	public:
-		void onUpdate(World& world, Actor& owner, float deltaTime);
 		void transitAnimationUpperBody(const Animation& animation, float transitionTime = 1.0f);
 		void transitAnimationLowerBody(const Animation& animation, float transitionTime = 1.0f);
+		void onUpdate(World& world, Actor& owner, float deltaTime);
 
 	private:
 		Layer _upperBody{};
 		Layer _lowerBody{};
+
+		//std::vector<Transform> _resultPose;
 	};
 }
